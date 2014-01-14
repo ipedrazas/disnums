1695804a0a783df249fed3a8075a2a25ef2a3090
Ivan Pedrazas <ipedrazas@gmail.com>
2014-01-14 01:45:36 +0000

    Activities to fullscreen

--- src/me/pedrazas/disnums/ResultActivity.java
+++ src/me/pedrazas/disnums/ResultActivity.java
@@ -6,6 +6,8 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class ResultActivity extends Activity {
@@ -13,6 +15,9 @@ public class ResultActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+	                            WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_result);
 		Intent intent = getIntent();
 		boolean success = intent.getBooleanExtra("SUCCESS", false);
